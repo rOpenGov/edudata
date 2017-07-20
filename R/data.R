d5d91dc7491a401c47fbba6ad61d99e02d3c9d74
Markus Kainu <markus.kainu@gmail.com>
2017-07-20 12:32:37 +0300

    datasets now processed as .rda in data folder and documented in R/data.R

---
+++ R/data.R
@@ -0,0 +1,136 @@
+#' Exercises on R basics
+#'
+#' This data provides basics exercises for edu-package
+#'
+#' @format yaml-file with :
+#' \describe{
+#'   \item{id}{unique id for each exercise}
+#'   \item{domain}{self-defined domain that exercise belongs to (affects the edu-package addin)}
+#'   \item{grade}{self-defined difficulty of the exercise. for instance easy, moderate or hard}
+#'   \item{lang_en}{Question in English}
+#'   \item{lang_fi}{Question in Finnish or any other language}
+#'   \item{extra}{Some extra preformatted content you want to be displayed with the exercise}
+#'   \item{ans}{Correct answer or multiple alternative answers}
+#' }
+#' @source \url{https://github.com/ropengov/edudata}
+"basics"
+
+#' Exercises on eurostat-package
+#'
+#' This data provides exercises for the use of eurostat-package for edu-package
+#'
+#' @format yaml-file with :
+#' \describe{
+#'   \item{id}{unique id for each exercise}
+#'   \item{domain}{self-defined domain that exercise belongs to (affects the edu-package addin)}
+#'   \item{grade}{self-defined difficulty of the exercise. for instance easy, moderate or hard}
+#'   \item{lang_en}{Question in English}
+#'   \item{lang_fi}{Question in Finnish or any other language}
+#'   \item{extra}{Some extra preformatted content you want to be displayed with the exercise}
+#'   \item{ans}{Correct answer or multiple alternative answers}
+#' }
+#' @source \url{https://github.com/ropengov/edudata}
+"eurostat"
+
+#' Exercises on R data exporting
+#'
+#' This data provides exercises for exporting data from R for edu-package
+#'
+#' @format yaml-file with :
+#' \describe{
+#'   \item{id}{unique id for each exercise}
+#'   \item{domain}{self-defined domain that exercise belongs to (affects the edu-package addin)}
+#'   \item{grade}{self-defined difficulty of the exercise. for instance easy, moderate or hard}
+#'   \item{lang_en}{Question in English}
+#'   \item{lang_fi}{Question in Finnish or any other language}
+#'   \item{extra}{Some extra preformatted content you want to be displayed with the exercise}
+#'   \item{ans}{Correct answer or multiple alternative answers}
+#' }
+#' @source \url{https://github.com/ropengov/edudata}
+"export"
+
+#' Exercises on R data importing
+#'
+#' This data provides exercises for importing data into R for edu-package
+#'
+#' @format yaml-file with :
+#' \describe{
+#'   \item{id}{unique id for each exercise}
+#'   \item{domain}{self-defined domain that exercise belongs to (affects the edu-package addin)}
+#'   \item{grade}{self-defined difficulty of the exercise. for instance easy, moderate or hard}
+#'   \item{lang_en}{Question in English}
+#'   \item{lang_fi}{Question in Finnish or any other language}
+#'   \item{extra}{Some extra preformatted content you want to be displayed with the exercise}
+#'   \item{ans}{Correct answer or multiple alternative answers}
+#' }
+#' @source \url{https://github.com/ropengov/edudata}
+"import"
+
+#' Exercises on statistical modelling in R
+#'
+#' This data provides exercises for statistical modelling in R for edu-package
+#'
+#' @format yaml-file with :
+#' \describe{
+#'   \item{id}{unique id for each exercise}
+#'   \item{domain}{self-defined domain that exercise belongs to (affects the edu-package addin)}
+#'   \item{grade}{self-defined difficulty of the exercise. for instance easy, moderate or hard}
+#'   \item{lang_en}{Question in English}
+#'   \item{lang_fi}{Question in Finnish or any other language}
+#'   \item{extra}{Some extra preformatted content you want to be displayed with the exercise}
+#'   \item{ans}{Correct answer or multiple alternative answers}
+#' }
+#' @source \url{https://github.com/ropengov/edudata}
+"model"
+
+#' Exercises on tidying data in R
+#'
+#' This data provides exercises for tidying data with R for edu-package
+#'
+#' @format yaml-file with :
+#' \describe{
+#'   \item{id}{unique id for each exercise}
+#'   \item{domain}{self-defined domain that exercise belongs to (affects the edu-package addin)}
+#'   \item{grade}{self-defined difficulty of the exercise. for instance easy, moderate or hard}
+#'   \item{lang_en}{Question in English}
+#'   \item{lang_fi}{Question in Finnish or any other language}
+#'   \item{extra}{Some extra preformatted content you want to be displayed with the exercise}
+#'   \item{ans}{Correct answer or multiple alternative answers}
+#' }
+#' @source \url{https://github.com/ropengov/edudata}
+"tidy"
+
+#' Exercises on transforming data in R
+#'
+#' This data provides exercises for transforming data with R for edu-package
+#'
+#' @format yaml-file with :
+#' \describe{
+#'   \item{id}{unique id for each exercise}
+#'   \item{domain}{self-defined domain that exercise belongs to (affects the edu-package addin)}
+#'   \item{grade}{self-defined difficulty of the exercise. for instance easy, moderate or hard}
+#'   \item{lang_en}{Question in English}
+#'   \item{lang_fi}{Question in Finnish or any other language}
+#'   \item{extra}{Some extra preformatted content you want to be displayed with the exercise}
+#'   \item{ans}{Correct answer or multiple alternative answers}
+#' }
+#' @source \url{https://github.com/ropengov/edudata}
+"transform"
+
+#' Exercises on visualising data in R
+#'
+#' This data provides exercises for visualising data with R for edu-package
+#'
+#' @format yaml-file with :
+#' \describe{
+#'   \item{id}{unique id for each exercise}
+#'   \item{domain}{self-defined domain that exercise belongs to (affects the edu-package addin)}
+#'   \item{grade}{self-defined difficulty of the exercise. for instance easy, moderate or hard}
+#'   \item{lang_en}{Question in English}
+#'   \item{lang_fi}{Question in Finnish or any other language}
+#'   \item{extra}{Some extra preformatted content you want to be displayed with the exercise}
+#'   \item{ans}{Correct answer or multiple alternative answers}
+#' }
+#' @source \url{https://github.com/ropengov/edudata}
+"visualise"
+
--- data-raw/process_data.R
+++ data-raw/process_data.R
@@ -1,3 +1,10 @@
+## This script processes the yml-files in ./data-raw/-folder and saves them in
+## rda-format in data-folder.
+## Documentation in ./R/-folder as data.R must be updated manually.
+
+# clean the environment
+rm(list = ls())
+# read all ymls into data.frame and assign object names corresponding with yml file names
 library(dplyr)
 flies <- list.files("./data-raw/",pattern = ".yml", full.names = TRUE)
 for (i in flies){
@@ -8,6 +15,8 @@ for (i in flies){
 }
 rm(tmp)
 
+# save each data.frame as .rds into ./data/-folder
+dir.create("./data", showWarnings = FALSE)
 dfs <- Filter(function(x) is.data.frame(get(x)) , ls())
 for(d in dfs) {
   save(list=d, file=paste0("./data/", d, ".rda"))
